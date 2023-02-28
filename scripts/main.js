ec43be276a8d0d86d439f56e932c8eaa11e565ea
unknown <freelancestanislav@gmail.com>
2023-02-28 19:34:32 +0300

    redesign navbar and main page

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,22 @@
 const navbar = document.querySelector("nav")
-const handle_button = document.getElementById("handle_button")
+// const handle_button = document.getElementById("handle_button")
 
-handle_button.addEventListener("click", () => {
-    if (navbar.classList.contains("show")) {
-        navbar.classList.remove("show")
-        handle_button.classList.remove("active")
+// handle_button.addEventListener("click", () => {
+//     if (navbar.classList.contains("show")) {
+//         navbar.classList.remove("show")
+//         handle_button.classList.remove("active")
+//     } else {
+//         navbar.classList.add("show")
+//         handle_button.classList.add("active")
+//     }
+// })
+function resizeNavbar() {
+    if (window.pageYOffset > 100) {
+        navbar.classList.add("onscroll")
     } else {
-        navbar.classList.add("show")
-        handle_button.classList.add("active")
+        navbar.classList.remove("onscroll")
     }
-})
+}
+
+window.addEventListener("scroll", resizeNavbar)
+resizeNavbar()
