3fc426f78c02cebc987389c3ecab1a63bf0c30dd
unknown <freelancestanislav@gmail.com>
2023-03-03 16:06:00 +0300

    add mobile navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
 const navbar = document.querySelector("nav")
 const consultationLine = document.getElementById("consultationLine")
+const toggleButton = document.getElementById("toggleButton")
+
+toggleButton.addEventListener("click", () => {
+    navbar.classList.contains('active')
+        ? navbar.classList.remove('active')
+        : navbar.classList.add('active')
+})
 
 function resizeNavbar() {
     consultationLine.style.top = ((180 - window.pageYOffset) > 0 ? 180 - window.pageYOffset : 0) + "px"
