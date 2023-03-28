28b1c8d39d7c618cd052dcf724987a294776001d
unknown <freelancestanislav@gmail.com>
2023-03-29 00:48:14 +0300

    fix navbar scroll on rotate mobile; add black wall on active navbar; update diplomas height from vh to px

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,9 +2,13 @@ const navbar = document.querySelector("nav")
 const toggleButton = document.getElementById("toggleButton")
 
 toggleButton.addEventListener("click", () => {
-    navbar.classList.contains('active')
-        ? navbar.classList.remove('active')
-        : navbar.classList.add('active')
+    if (navbar.classList.contains('active')) {
+        navbar.classList.remove('active')
+        document.body.classList.remove("mobileNavbarActive")
+    } else {
+        document.body.classList.add("mobileNavbarActive")
+        navbar.classList.add('active')
+    }
 })
 
 const resizeNavbar = () => {
